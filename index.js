cb0a455d6c14c5ac177d7e0bda0a45846e64921a
Sduttt <subhamdutta588@gmail.com>
2023-03-31 09:29:42 +0530

    bug fix 2

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 4000;
-app.get("/", function (req, res) {
-    res.status(200).send("<h1>Hello from Subham!</h1>");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Hello from Subham!</h1>");
 });
-app.get("/api/v1/linkedin", function (req, res) {
-    var linkedin = {
-        id: "its_subham_dutta",
-        following: 115,
-        follower: 90
-    };
-    res.status(200).json(linkedin);
+
+app.get("/api/v1/linkedin", (req, res) => {
+  const linkedin = {
+    id: "its_subham_dutta",
+    following: 115,
+    follower: 90,
+  };
+
+  res.status(200).json(linkedin);
 });
-app.get("/api/v1/twitter", function (req, res) {
-    var twitter = {
-        id: "subhamd588",
-        following: 150,
-        follower: 10
-    };
-    res.status(200).json(twitter);
+
+app.get("/api/v1/twitter", (req, res) => {
+  const twitter = {
+    id: "subhamd588",
+    following: 150,
+    follower: 10,
+  };
+
+  res.status(200).json(twitter);
 });
-app.get("/api/v1/github", function (req, res) {
-    var github = {
-        id: "Sduttt",
-        following: 4,
-        follower: 3
-    };
-    res.status(200).json(github);
+
+app.get("/api/v1/github", (req, res) => {
+  const github = {
+    id: "Sduttt",
+    following: 4,
+    follower: 3,
+  };
+
+  res.status(200).json(github);
 });
-app.get("/api/v1/:token", function (req, res) {
-    res.status(200).send(req.params.token);
+
+app.get("/api/v1/:token", (req, res) => {
+  res.status(200).send(req.params.token);
 });
-app.listen(PORT, function () {
-    console.log("server is running at port ".concat(PORT));
+
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
 });
